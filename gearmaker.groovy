0bd6de51d9d8aba8791b48095e4e3c0b5b665f7d
slitovchick <slitovchick@fi103-10.admin.wpi.edu>
2018-10-10 18:33:51 -0400

    Added D shaft
    Cut out the proper size pololu d shaft from the 12 tooth gear.

--- gearmaker.groovy
+++ gearmaker.groovy
@@ -1,1 +1,27 @@
 //Your code here
+
+File servoFile = ScriptingEngine.fileFromGit(
+	"https://github.com/sonchika/gear_d_shaft_12.git",
+	"12_TOOTH_GEAR_D_SHAFT_BOWLER.STL");
+// Load the .CSG from the disk and cache it in memory
+
+CSG gear  = Vitamins.get(servoFile)
+			.toZMin()
+			.toXMin()
+			.toYMin()
+double totalX = gear.getTotalX()
+println "X dim "+totalX
+double totalY = gear.getTotalY()
+println "Y dim "+totalY
+
+gear=gear
+	.movex(-totalX/2)
+	.movey(-totalX/2)
+
+CSG myDShaft = Vitamins.get("dShaft","WPI-gb37y3530-50en");
+
+myDShaft=myDShaft.union(myDShaft.movez(myDShaft.getTotalZ()))
+gear=gear.difference(myDShaft)
+
+
+return [gear]
